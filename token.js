80a21339c97b6c3bed9006f69d55c16879100b45
Joseph Faulkner <joebfaulkner@gmail.com>
2022-03-09 23:39:36 -0600

    Update token.js
    Finished first draft of the tokenizer. Could still make modifications to the token types and add keywords.

--- token.js
+++ token.js
@@ -40,11 +40,11 @@ class Token
                 {
                     previousSpot = currentSpot;
                     currentSpot++;
-                    while(input.charAt(currentSpot) != "/" && input.charAt(currentSpot-1) != "*")
+                    while(input.charAt(currentSpot) != "/" || input.charAt(currentSpot-1) != "*")
                     {
                         currentSpot++;
                     }
-                    let tokenName = input.slice(previousSpot, currentSpot);
+                    let tokenName = input.slice(previousSpot-1, currentSpot+1);
                     tokens.push(new Token(tokenName, "commment"));
                     previousSpot = currentSpot;
                 }
@@ -61,7 +61,25 @@ class Token
                 tokens.push(new Token(tokenName, "literal"));
                 previousSpot = currentSpot;
             }
-            
+            else if(opperators.indexOf(input.charAt(currentSpot)) !== -1 && !(seperators.indexOf(input.charAt(currentSpot-1)) !== -1))
+            {
+                let tokenName = input.slice(previousSpot + 1, currentSpot);
+                if(tokenName != '')
+                {
+                    tokens.push(new Token(tokenName, "identifier"));
+                    if(opperators.indexOf(input.charAt(currentSpot) + input.charAt(currentSpot+1)) !== -1)
+                    {
+                        tokenName = input.slice(currentSpot, currentSpot+2)
+                        tokens.push(new Token(tokenName, "opperator"));
+                    }
+                    else
+                    {
+                        tokenName = input.charAt(currentSpot)
+                        tokens.push(new Token(tokenName, "opperator"));
+                    }
+                    previousSpot = currentSpot;
+                }
+            }
             else if(seperators.indexOf(input.charAt(currentSpot)) !== -1 || currentSpot == input.length) //Check the current spot for seperators or if we've reached the end of the input
             {
                 let tokenName = input.slice(previousSpot + 1, currentSpot);
@@ -84,15 +102,32 @@ class Token
                 }
                 else if(tokenName !== '') // If it's none of that it should be an identifier
                 {
+                    /*let x = 0;
+                    while(x < tokenName.length) // Check if they have an opperator hidden between an identifier and a seperator like: int x=0;
+                    {
+                        if(operators.indexOf(tokenName.charAt(x)) !== -1)
+                        {
+                            let temp = tokenName.slice(0,x);
+                            tokens.push(Token(temp, "identifier"));
+                            let y = x;
+                            while(y < tokenName.length)
+                            {
+                                if(operators.indexOf(tokenName.charAt(y)) !== -1)
+                                {
+
+                                    break;
+                                }
+                                y++;
+                            }
+                            break;
+                        }
+                        x++
+                    }*/
                     if(tokenName.slice(tokenName.length - 2, tokenName.length) === '++' || tokenName.slice(tokenName.length - 2, tokenName.length) === '--') // Cover instances such as x++ or x--
                     {
                         tokens.push(new Token(tokenName.slice(0, tokenName.length -2), "identifier"));
                         tokens.push(new Token(tokenName.slice(tokenName.length-2, tokenName.length), "opperator"));
                     }
-                    /*else if(tokenName.search(opperators) !== -1)
-                    {
-                        console.log("this actually works");
-                    }*/
                     else
                     {
                         tokens.push(new Token(tokenName, "identifier"));
